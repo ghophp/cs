ec31c88f1772a2f14f4c5c60d9605cb4ea1c34ad
Guilherme Oliveira <ghophp@gmail.com>
2017-11-27 09:19:55 +0100

    finish sum in range with interesting technique

--- codefight/sum_in_range.go
+++ codefight/sum_in_range.go
@@ -5,36 +5,24 @@ import (
 	"math"
 )
 
-var mod10plus7 = int(math.Pow(10, 9) + 7)
-
-func hashIt(x, y int) string {
-	return fmt.Sprintf("%d.%d", x, y)
-}
-
-// if I know the sum from 0,10 the sum from 3,7 is inside of that index, so I need
-// to figure it out how to take this difference
+var (
+	mod10plus7 = int(math.Pow(10, 9) + 7)
+)
 
 func sumInRange(nums []int, queries [][]int) int {
 	var (
-		hashSet = map[string]int{}
-		sum     int
+		prefix = make([]int, len(nums)+1)
+		sum    int
 	)
 
-	for x := 0; x < len(queries); x++ {
-		var hashed string = hashIt(queries[x][0], queries[x][1])
-		if v, ok := hashSet[hashed]; ok {
-			sum = sum + v
-			continue
-		}
+	for x := 0; x < len(nums); x++ {
+		sum = sum + nums[x]
+		prefix[x+1] = sum
+	}
 
-		var innerSum int
-		for i := queries[x][0]; i <= queries[x][1]; i++ {
-			innerSum = innerSum + nums[i]
-		}
-
-		hashSet[hashed] = innerSum
-
-		sum = sum + innerSum
+	sum = 0
+	for _, query := range queries {
+		sum = sum + (prefix[query[1]+1] - prefix[query[0]])
 	}
 
 	return (sum + mod10plus7) % mod10plus7
